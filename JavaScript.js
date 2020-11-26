aab30813dfb72378d9576f888a1dc85c7d17fb2e
iENCEE <75098760+iENCEE@users.noreply.github.com>
2020-11-26 17:57:00 -0500

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,24 @@
+function index(){
+	var ibutt=document.getElementById("indexbutton");
+	ibutt.style.display ="none";
+	document.getElementById("index").style.display = "block";	
+}
+
+function twitch(){
+	var tbutt=document.getElementById("twitchbutton");
+	tbutt.style.display ="none";
+	document.getElementById("twitch").style.display = "block";
+	document.getElementsByName("i").style.display = "block";
+}
+
+function basketball(){
+	var bbutt=document.getElementById("basketballbutton");
+	bbutt.style.display ="none";
+	document.getElementById("basketball").style.display = "block";	
+}
+
+function resume(){
+	var rbutt=document.getElementById("resumebutton");
+	rbutt.style.display ="none";
+	document.getElementById("resume").style.display = "block";	
+}
